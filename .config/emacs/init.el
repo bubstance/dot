f26f9fdef25490b7c24b8a23ba2d83bfea928262
Kelly Roberts <kelly.glenn.roberts@gmail.com>
2022-12-11 03:12:36 -0700

    tweak emacs base conf

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -9,37 +9,3 @@
 
 (setq custom-file (locate-user-emacs-file "custom-vars.el"))
 (load custom-file 'noerror 'nomessage)
-
-;; install straight.el and configure some base packages
-(defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 6))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
-
-(straight-use-package 'use-package)
-
-(use-package el-patch
-  :straight t)
-
-(use-package evil
-  :hook
-    (after-init . evil-mode)
-  :init
-  (setq evil-want-integration t) ;; This is optional since it's already set to t by default.
-  (setq evil-want-keybinding nil)
-  :config
-  (evil-mode 1))
-
-(use-package evil-collection
-  :after evil
-  :straight t
-  :config
-  (evil-collection-init))
